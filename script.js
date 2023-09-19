9cab518fe7e385437e130c7f3236465079acd40b
JohnA28 <John.Alban41@gmail.com>
2023-09-19 08:35:22 -0400

    added variables and functions. just a basic skeleton

---
+++ script.js
@@ -0,0 +1,26 @@
+const sign = [1,2,3];
+//1 - rock
+//2 - paper
+//3 - scissors
+const playerSelection = "";
+const computerSelection = getComputerChoice()
+
+//rng a choice for the comp to use against the player
+function getComputerChoice() {
+}
+
+//returns the result of a round
+//prompt() gets user input
+function playRound(playerSelection, computerSelection){
+}
+
+//this will play 5 rounds of rock paper scissors
+//just use a while loop that incrimentes after every round
+function game(){
+}
+
+
+//result messages 
+
+console.log("You Lose!")
+console.log("You Win!")
