a56e7699bed635b7be25e8fdfc6730430a1b6591
JohnA28 <John.Alban41@gmail.com>
2023-09-19 22:51:35 -0400

    added play round function. setup the logic for a round of rps, no user input yet

--- script.js
+++ script.js
@@ -1,26 +1,70 @@
-const sign = [1,2,3];
+const signs = [1,2,3];
 //1 - rock
 //2 - paper
 //3 - scissors
-const playerSelection = "";
+
+const playerSelection = "paper";
 const computerSelection = getComputerChoice()
 
 //rng a choice for the comp to use against the player
 function getComputerChoice() {
+  return signs[Math.floor(Math.random()*signs.length)];
 }
 
 //returns the result of a round
 //prompt() gets user input
 function playRound(playerSelection, computerSelection){
+  switch(playerSelection.toLowerCase()) {
+    case "rock":
+      if (computerSelection === signs[0]) {
+        return('Tie!')
+      }
+      else if (computerSelection === signs[1]) {
+        return('You Lose!')
+      }
+      else if (computerSelection === signs[2]) {
+        return('You Win!')
+      }
+      break;
+
+    case "paper":
+      if (computerSelection === signs[0]) {
+        return('You Win!')
+      }
+      else if (computerSelection === signs[1]) {
+        return('Tie!')
+      }
+      else if (computerSelection === signs[2]) {
+        return('You Lose!')
+      }
+      break;
+
+      case "scissors":
+        if (computerSelection === signs[0]) {
+          return('You Lose!')
+        }
+        else if (computerSelection === signs[1]) {
+          return('You Win!')
+        }
+        else if (computerSelection === signs[2]) {
+          return ('Tie!')
+        }
+        break;
+
+      default:
+        return('Please choose Rock, Paper, or Scissors')
+  }
+
 }
+
+
 
 //this will play 5 rounds of rock paper scissors
 //just use a while loop that incrimentes after every round
 function game(){
+  playRound()
 }
 
 
-//result messages 
-
-console.log("You Lose!")
-console.log("You Win!")
+//result messagesnode
+console.log(playRound(playerSelection,computerSelection))
